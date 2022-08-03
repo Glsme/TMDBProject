9692b1bbf8311c37de29fdec7dbebd86bb094d5d
Glsme <glasses.str.man@gmail.com>
2022-08-03 16:31:50 +0900

    Feat: 네트워크 통신 연결 추가 및 확인 작업

--- TMDBProject/ListViewController.swift
+++ TMDBProject/ListViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 
+import Alamofire
+import Kingfisher
+import SwiftyJSON
+
 class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        requestTMDB(media_type: "all", time_window: "week")
 
+    }
+    
+    func requestTMDB(media_type: String, time_window: String) {
+        let url = "\(EndPoint.TMDBURL)" + "\(media_type)/" + "\(time_window)?" + "api_key=\(APIKey.TMDB)"
+        AF.request(url, method: .get).validate(statusCode: 200...500).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 }
+
 
