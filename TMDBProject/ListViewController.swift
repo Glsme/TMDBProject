8479a2632352f9e4f6b0bb42d8b11abc235f23d0
Glsme <glasses.str.man@gmail.com>
2022-08-03 16:17:38 +0900

    Feat: 기본 UI 구현

--- TMDBProject/ListViewController.swift
+++ TMDBProject/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListViewController.swift
 //  TMDBProject
 //
 //  Created by Seokjune Hong on 2022/08/03.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
 
 }
 
