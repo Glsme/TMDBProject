6cc36305515a244578d0403e533247c9519ec3ec
Glsme <glasses.str.man@gmail.com>
2022-08-07 21:46:05 +0900

    Feat: Web 관련 파일 추가

---
+++ TMDBProject/Web/WebViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebViewController.swift
+//  TMDBProject
+//
+//  Created by Seokjune Hong on 2022/08/07.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
