1c15959f73b760baa8da630d72635d17e88be57f
Glsme <glasses.str.man@gmail.com>
2022-08-11 21:59:00 +0900

    Feat: 위치 권한 설정 추가

--- TMDBProject/Theater/TheaterViewController.swift
+++ TMDBProject/Theater/TheaterViewController.swift
@@ -6,12 +6,89 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import AVFoundation
 
 class TheaterViewController: UIViewController {
 
+    @IBOutlet weak var theaterMapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
 
+        self.navigationController?.navigationBar.topItem?.title = ""
+        self.navigationController?.navigationBar.tintColor = .black
+        
+        checkUserDeviceLocationServiceAuthorization()
     }
+    
+    func setRegionAndAnnotation(center: CLLocationCoordinate2D) {
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        theaterMapView?.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        annotation.title = "현재 위치"
+        
+        theaterMapView.addAnnotation(annotation)
+    }
+}
 
+extension TheaterViewController {
+    func checkUserDeviceLocationServiceAuthorization() {
+        let authorizationStatus: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            print("이 디바이스는 iOS 14.0 이상입니다.")
+            authorizationStatus = locationManager.authorizationStatus
+        } else {
+            print("이 디바이스는 iOS 14.0 미만입니다.")
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            checkUserCurrentLocationAuthorization(authorizationStatus)
+        } else {
+            print("위치 서비스가 꺼져 있어서 위치 권한 요청을 하지 못합니다.")
+        }
+    }
+    
+    func checkUserCurrentLocationAuthorization(_ authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .notDetermined:
+            print("NOT DETERMINED")
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            print("DENIED, 아이폰 설정으로 유도합니다.")
+        case .authorizedWhenInUse:
+            print("WHEN IN USE")
+            locationManager.startUpdatingLocation()
+        default:
+            print("DEFUALT")
+        }
+    }
+}
+
+extension TheaterViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.last?.coordinate {
+            setRegionAndAnnotation(center: coordinate)
+        }
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkUserDeviceLocationServiceAuthorization()
+    }
 }
