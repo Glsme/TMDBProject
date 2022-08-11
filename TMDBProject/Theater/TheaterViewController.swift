09c93d27b2697936ea8e7a47662328abfc9d9948
Glsme <glasses.str.man@gmail.com>
2022-08-11 21:17:26 +0900

    Feat: 라이트모드만 사용 가능하게 추가

---
+++ TMDBProject/Theater/TheaterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TheaterViewController.swift
+//  TMDBProject
+//
+//  Created by Seokjune Hong on 2022/08/11.
+//
+
+import UIKit
+
+class TheaterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
